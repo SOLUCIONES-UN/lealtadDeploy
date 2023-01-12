b8bdb16492c84e0dea3c2aeaac567bd3588c6f28
Jarny Estiven <estiven6647@gmail.com>
2023-01-12 15:56:20 -0600

    se implemento la edicion de las promosiones

--- js/promociones.js
+++ js/promociones.js
@@ -151,6 +151,47 @@ $(function () {
 
   })
 
+  $('#formEdit').submit(function() {
+      var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const id = $('#id').val();
+
+        var raw = JSON.stringify({
+          "nemonico": $('#nemonicoEdit').val(),
+          "nombre": $('#nombreEdit').val(),
+          "descripcion": $('#descripcionEdit').val(),
+          "mesajeExito": $('#successaMessageEdit').val(),
+          "mesajeFail": $('#failMessageEdit').val(),
+          "fechaInicio": $('#fechaInicioEdit').val(),
+          "fechaFin": $('#fechaFinEdit').val(),
+      });
+
+      var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch(`${url}Promocion/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                
+                if(result.code == "ok"){
+                  getAllPromociones();
+                    $('#modalEdit').modal('toggle');
+                    Alert(result.message, 'success');
+                } else {
+                    
+                    Alert(result.message, 'error')
+                }
+            })
+            .catch(error => {Alert(error.errors, 'error')});
+        return false;
+
+  })
+
   $('#BtnPremios').click(function () {
     var cantidad = $('#cantidaPremio').val();
     var premio = $('#premio').val();
@@ -235,7 +276,6 @@ const table = (table, data) => {
       },
       {
         data: "id", render: function (data, type, row) {
-          console.log(row.estado)
 
           var opcAdd = ``;
 
@@ -243,7 +283,7 @@ const table = (table, data) => {
 
           switch (row.estado) {
             case 1:
-              opcAdd += `<a href="#" onclick="UpdatePromocion(${data},2)" class="btn_delete dropdown-item">
+              opcAdd += `<a href="#" onclick="OpenEdit(${data})" class="btn_delete dropdown-item">
               ${feather.icons['pause-circle'].toSvg({ class: 'font-small-4 mr-50' })} Pausar
             </a>`
               break;
@@ -455,13 +495,110 @@ const removePremio = (index) => {
   DrawPremios()
 }
 
+const loadMenuEdit = () => {
+  var bsStepper = document.querySelectorAll('.bs-stepper'),
+    select = $('.select2'),
+    verticalWizard = document.querySelector('.vertical-wizard-example-Edit');
+
+
+  // Adds crossed class
+  if (typeof bsStepper !== undefined && bsStepper !== null) {
+    for (var el = 0; el < bsStepper.length; ++el) {
+      bsStepper[el].addEventListener('show.bs-stepper', function (event) {
+        var index = event.detail.indexStep;
+        var numberOfSteps = $(event.target).find('.step').length - 1;
+        var line = $(event.target).find('.step');
+        console.log(numberOfSteps)
+        // The first for loop is for increasing the steps,
+        // the second is for turning them off when going back
+        // and the third with the if statement because the last line
+        // can't seem to turn off when I press the first item. ¯\_(ツ)_/¯
+
+        for (var i = 0; i < index; i++) {
+          line[i].classList.add('crossed');
+
+          for (var j = index; j < numberOfSteps; j++) {
+            line[j].classList.remove('crossed');
+          }
+        }
+        if (event.detail.to == 0) {
+          for (var k = index; k < numberOfSteps; k++) {
+            line[k].classList.remove('crossed');
+          }
+          line[0].classList.remove('crossed');
+        }
+      });
+    }
+  }
+
+  // select2
+  select.each(function () {
+    var $this = $(this);
+    $this.wrap('<div class="position-relative"></div>');
+    $this.select2({
+      placeholder: 'Select value',
+      dropdownParent: $this.parent()
+    });
+  });
+
+
+  // Vertical Wizard
+  // --------------------------------------------------------------------
+  if (typeof verticalWizard !== undefined && verticalWizard !== null) {
+    var verticalStepper = new Stepper(verticalWizard, {
+      linear: false
+    });
+    $(verticalWizard)
+      .find('.btn-next')
+      .on('click', function () {
+        $('#text-nemonico').text($('#nemonico').val());
+        $('#text-nombre').text($('#nombre').val());
+        $('#text-descripcion').text($('#descripcion').val());
+        $('#text-success').text($('#successaMessage').val());
+        $('#text-fail').text($('#failMessage').val());
+        $('#text-fechaInicio').text($('#fechaInicio').val());
+        $('#text-fechaFin').text($('#fechaFin').val());
+        verticalStepper.next();
+      });
+    $(verticalWizard)
+      .find('.btn-prev')
+      .on('click', function () {
+        verticalStepper.previous();
+      });
+  }
+}
+
+const OpenEdit = (id) => {
+  var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+  };
+
+  fetch(`${url}Promocion/${id}`, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+          $('#id').val(id);
+          $('#nemonicoEdit').val(result.nemonico);
+          $('#nombreEdit').val(result.nombre); 
+          $('#descripcionEdit').val(result.descripcion);
+          $('#successaMessageEdit').val(result.mesajeExito);
+          $('#failMessageEdit').val(result.mesajeFail);
+          $('#fechaInicioEdit').val(result.fechaInicio);
+          $('#fechaFinEdit').val(result.fechaFin);
+          $('#modalEdit').modal('toggle');
+      })
+      .catch(error => console.log('error', error));
+      loadMenuEdit();
+}
+
 const UpdatePromocion = (id, type) => {
+  OpenEdit(id)
   var requestOptions = {
     method: 'PUT',
     redirect: 'follow'
   };
 
-  fetch(`${url}Promocion/${type == 1 ? 'Act' : 'Pau'}/${id}`, requestOptions)
+  fetch(`${url}Promocion/${type == 1 ? 'Act' : 'Pau'}/?id=${id}`, requestOptions)
     .then(response => response.json())
     .then(result => {
       if (result.code == "ok") {
