97c62c9ff69664e0d6efa15a40d0b491a3db7982
Estiven Alvarez <estiven6647mail.com>
2024-03-15 00:02:11 -0600

    correccion de cards de referidos

--- js/configReferidos.js
+++ js/configReferidos.js
@@ -1,47 +1,80 @@
 const url = "http://localhost:3000/";
 let token = localStorage.getItem("token");
 const headers = {
-  'Authorization': token,
-  'Content-Type': 'application/json'
+  Authorization: token,
+  "Content-Type": "application/json",
 };
-
 
 $(function () {
   obtenerData();
 
-$("#swFacebook").change(function () {
-  chechFacebook();
-});
-$("#swInstagram").change(function () {
-  chechInstagram();
-});
-$("#swWhatsapp").change(function () {
-  chechWhatsApp();
-});
-$("#swMensaje").change(function () {
-  chechMensajes();
-});
-$("#swPantalla").change(function () {
-  chechPantalla();
-});
+  $("#swFacebook").change(function () {
+    chechFacebook();
+    if ($("#swFacebook").prop("checked")) {
+      //habilitar la card de facebook
+      DeleteConfigReferidos(1, 1);
+    } else {
+      //deshabilitar la card de facebook
+      DeleteConfigReferidos(1, 0);
+    }
+  });
+  $("#swInstagram").change(function () {
+    chechInstagram();
+    if ($("#swFacebook").prop("checked")) {
+      //habilitar la card de facebook
+      DeleteConfigReferidos(2, 1);
+    } else {
+      //deshabilitar la card de facebook
+      DeleteConfigReferidos(2, 0);
+    }
+  });
+  $("#swWhatsapp").change(function () {
+    chechWhatsApp();
+    if ($("#swFacebook").prop("checked")) {
+      //habilitar la card de facebook
+      DeleteConfigReferidos(3, 1);
+    } else {
+      //deshabilitar la card de facebook
+      DeleteConfigReferidos(3, 0);
+    }
+  });
+  $("#swMensaje").change(function () {
+    chechMensajes();
+    if ($("#swFacebook").prop("checked")) {
+      //habilitar la card de facebook
+      DeleteConfigReferidos(4, 1);
+    } else {
+      //deshabilitar la card de facebook
+      DeleteConfigReferidos(4, 0);
+    }
+  });
+  $("#swPantalla").change(function () {
+    chechPantalla();
+    if ($("#swFacebook").prop("checked")) {
+      //habilitar la card de facebook
+      DeleteConfigReferidos(5, 1);
+    } else {
+      //deshabilitar la card de facebook
+      DeleteConfigReferidos(5, 0);
+    }
+  });
 
- //guardar texto
- $("#btnFacebook").click(function () {
-   updateConfigReferidos(1, $(textFacebook).val());
- });
- $("#btnInstagram").click(function () {
-  updateConfigReferidos(2, $(textInstagram).val());
- });
- $("#btnWhatsapp").click(function () {
-   updateConfigReferidos(3, $(textWhatsapp).val());
- });
- $("#btnMensaje").click(function () {
-   updateConfigReferidos(4, $(textMensaje).val());
- });
- $("#btnPantalla").click(function () {
-   updateConfigReferidos(5, $(textPantalla).val());
- });
-
+  //guardar texto
+  $("#btnFacebook").click(function () {
+    updateConfigReferidos(1, $(textFacebook).val());
+  });
+  $("#btnInstagram").click(function () {
+    updateConfigReferidos(2, $(textInstagram).val());
+  });
+  $("#btnWhatsapp").click(function () {
+    updateConfigReferidos(3, $(textWhatsapp).val());
+  });
+  $("#btnMensaje").click(function () {
+    updateConfigReferidos(4, $(textMensaje).val());
+  });
+  $("#btnPantalla").click(function () {
+    updateConfigReferidos(5, $(textPantalla).val());
+  });
 });
 const Usuario = () => {
   let usuario = JSON.parse(localStorage.getItem("infoUsuario"));
@@ -66,7 +99,7 @@ const chechFacebook = () => {
     $("#pnFacebook").removeClass("bg-info");
     $("#pnFacebook").addClass("bg-secondary");
   }
- };
+};
 
 const chechInstagram = () => {
   if ($("#swInstagram").prop("checked")) {
@@ -86,9 +119,7 @@ const chechInstagram = () => {
   }
 };
 
-
 const chechWhatsApp = () => {
-
   if ($("#swWhatsapp").prop("checked")) {
     $("#whatssapxxx").prop("disabled", false);
     $("#btnWhatsapp").prop("disabled", false);
@@ -106,54 +137,49 @@ const chechWhatsApp = () => {
   }
 };
 
- const chechMensajes = () => {   
+const chechMensajes = () => {
   if ($("#swMensaje").prop("checked")) {
     $("#mensajexxx").prop("disabled", false);
     $("#btnMensaje").prop("disabled", false);
-     $("#btnMensaje").removeClass("btn-secondary");
-     $("#btnMensaje").addClass("btn-primary");
-     $("#pnMensaje").removeClass("bg-secondary");
-     $("#pnMensaje").addClass("bg-primary");
-   } else {
-     $("#mensaje").prop("disabled", true);
-     $("#btnMensaje").prop("disabled", true);
-     $("#btnMensaje").removeClass("btn-primary");
-     $("#btnMensaje").addClass("btn-secondary");
-     $("#pnMensaje").removeClass("bg-primary");
-     $("#pnMensaje").addClass("bg-secondary");
-   }
- };
- 
+    $("#btnMensaje").removeClass("btn-secondary");
+    $("#btnMensaje").addClass("btn-primary");
+    $("#pnMensaje").removeClass("bg-secondary");
+    $("#pnMensaje").addClass("bg-primary");
+  } else {
+    $("#mensaje").prop("disabled", true);
+    $("#btnMensaje").prop("disabled", true);
+    $("#btnMensaje").removeClass("btn-primary");
+    $("#btnMensaje").addClass("btn-secondary");
+    $("#pnMensaje").removeClass("bg-primary");
+    $("#pnMensaje").addClass("bg-secondary");
+  }
+};
 
 const chechPantalla = () => {
-   if ($("#swPantalla").prop("checked"));
-     $("#pantalla").prop("disabled", false);
+  if ($("#swPantalla").prop("checked"));
+  $("#pantalla").prop("disabled", false);
   if ($("#swPantalla").prop("checked")) {
     $("#pantallaxxx").prop("disabled", false);
     $("#btnPantalla").prop("disabled", false);
-     $("#btnPantalla").removeClass("btn-secondary");
-     $("#btnPantalla").addClass("btn-warning");
-     $("#pnPantalla").removeClass("bg-secondary");
-     $("#pnPantalla").addClass("bg-warning");
-   } else {
-     $("#pantalla").prop("disabled", true);
-     $("#btnPantalla").prop("disabled", true);
-     $("#btnPantalla").removeClass("btn-warning");
-     $("#btnPantalla").addClass("btn-secondary");
-     $("#pnPantalla").removeClass("bg-warning");
-     $("#pnPantalla").addClass("bg-secondary");
-   }
-  };
-
-
-
+    $("#btnPantalla").removeClass("btn-secondary");
+    $("#btnPantalla").addClass("btn-warning");
+    $("#pnPantalla").removeClass("bg-secondary");
+    $("#pnPantalla").addClass("bg-warning");
+  } else {
+    $("#pantalla").prop("disabled", true);
+    $("#btnPantalla").prop("disabled", true);
+    $("#btnPantalla").removeClass("btn-warning");
+    $("#btnPantalla").addClass("btn-secondary");
+    $("#pnPantalla").removeClass("bg-warning");
+    $("#pnPantalla").addClass("bg-secondary");
+  }
+};
 
 const obtenerData = () => {
-
   var requestOptions = {
     method: "GET",
     headers: {
-      'Authorization': token,
+      Authorization: token,
     },
     redirect: "follow",
   };
@@ -167,7 +193,8 @@ const obtenerData = () => {
             $("#textFacebook").val(element.descripcion);
             $("#swFacebook").prop(
               "checked",
-              element.estado == 0 ? false : true,);
+              element.estado == 0 ? false : true
+            );
             break;
           case 2:
             $("#textInstagram").val(element.descripcion);
@@ -204,23 +231,11 @@ const obtenerData = () => {
         chechPantalla();
       });
     })
-    .catch((error) => console.log("error", error))
-
+    .catch((error) => console.log("error", error));
 };
 
-
 const updateConfigReferidos = (id, descripcion) => {
-  // const boton = $("#tuBotonId");
 
-  // // Si el botón está deshabilitado, significa que la solicitud está en progreso
-  // if (boton.prop("disabled")) {
-  //   return;
-  // }
-
-  // boton.prop("disabled", true); 
-
-  console.log("Valid", id);
-  $("#tuBotonId").prop("disabled", true);
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Authorization", token);
@@ -239,59 +254,43 @@ const updateConfigReferidos = (id, descripcion) => {
         console.log("Resultado", result);
         CreateConfigReferidos(descripcion, id);
         obtenerData();
-
+        return;
       } else {
         console.log("", result);
-        CreateConfigReferidos(id, descripcion);  // Utiliza result.message en lugar de error
+        //CreateConfigReferidos(id, descripcion); // Utiliza result.message en lugar de error
       }
     })
     .catch((error) => console.log("error", error))
     .finally(() => {
       $("#tuBotonId").prop("disabled", false);
     });
-// const id = $('#id').val();
+  // const id = $('#id').val();
 
-       var raw = JSON.stringify({
-       descripcion: descripcion,
-       
-     });
+  var raw = JSON.stringify({
+    descripcion: descripcion,
+  });
 
-     var requestOptions = {
-         method: "PUT",
-        headers: myHeaders,
-          body: raw,
-         redirect: "follow",
-       };
-      fetch( `${url}ConfigReferidos/${id}`, requestOptions)
-     .then((response) => response.json())
-     .then((result) => {
-         if (result.code === "ok") {
+  var requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  fetch(`${url}ConfigReferidos/${id}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.code === "ok") {
         Alert(result.message, "success");
-     } else {
-        console.log("error", result);  // Utiliza result.message en lugar de error
-     }
-   })
-   boton.prop("disable", false); 
- }
- function Alert(message, status) {
-  try {
-    if (typeof toastr[status] === 'function') {
-      toastr[status](message, status, {
-        closeButton: true,
-        tapToDismiss: false,
-        positionClass: 'toast-top-right',
-        rtl: false,
-      });
-    } else {
-      console.error(` ${status}`);
-    }
-  } catch (error) {
-    console.error('', error);
-  }
-}
+      } else {
+        console.log("error", result); // Utiliza result.message en lugar de error
+      }
+    });
+  boton.prop("disable", false);
+};
 
-const CreateConfigReferidos = (id,descripcion, estado) => {
 
+
+const CreateConfigReferidos = (id, descripcion, estado) => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Authorization", token);
@@ -299,8 +298,7 @@ const CreateConfigReferidos = (id,descripcion, estado) => {
   var raw = JSON.stringify({
     descripcion: descripcion,
     estado: estado,
-        id: id
-
+    id: id,
   });
 
   var requestOptions = {
@@ -316,22 +314,20 @@ const CreateConfigReferidos = (id,descripcion, estado) => {
       if (result.code === "ok") {
         Alert(result.message, "Exitoso");
       } else {
-        console.log("error", result);  // Utiliza result.message en lugar de error
+        console.log("error", result); // Utiliza result.message en lugar de error
       }
-    })
+    });
 
-    $('#btnSubmit').prop('disabled', false);
-    Alert(error.errors, 'error');
-   
-
-}
-const DeleteConfigReferidos = (id) => {
+  $("#btnSubmit").prop("disabled", false);
+  Alert(error.errors, "error");
+};
+const DeleteConfigReferidos = (id, estado) => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Authorization", token);
 
   var raw = JSON.stringify({
-    id: id,
+    estado: estado,
   });
 
   var requestOptions = {
@@ -341,104 +337,107 @@ const DeleteConfigReferidos = (id) => {
     redirect: "follow",
   };
 
-  fetch(`${url}ConfigReferido/${id}`, requestOptions)
+  fetch(`${url}ConfigReferidos/${id}`, requestOptions)
     .then((response) => response.json())
     .then((result) => {
+      console.log("este es el result", result);
       if (result.code === "ok") {
-        Alert(result.message, "Exitoso");
+        Alert(result.message, "success");
       } else {
-        console.log("error", result);  // Utiliza result.message en lugar de error
+        console.log("error", result); // Utiliza result.message en lugar de error
       }
     })
     .catch((error) => console.log("error", error));
+};
 
+// $(document).ready(function () {
+//   // Evento de clic en la tarjeta de Facebook
+//   $("#pnFacebook").click(function () {
+//     // Cambiar el estado del switch al contrario del estado actual
+//     $("#swFacebook").prop("checked", !$("#swFacebook").prop("checked"));
+//     // Llamar a la función para ajustar el estado visual
+//     chechFacebook();
+//   });
+// });
+
+$(document).ready(function () {
+  // Evento de clic en el texto asociado a Facebook
+  $("#textFacebook").click(function () {
+    // Verificar el estado actual del switch
+    if (!$("#swFacebook").prop("checked")) {
+      // Cambiar el estado del switch solo si está apagado
+      $("#swFacebook").prop("checked", true);
+      // Llamar a la función para ajustar el estado visual
+      chechFacebook();
     }
-    
-    $(document).ready(function() {
-      // Evento de clic en la tarjeta de Facebook
-      $("#pnFacebook").click(function() {
-          // Cambiar el estado del switch al contrario del estado actual
-          $("#swFacebook").prop("checked", !$("#swFacebook").prop("checked"));
-          // Llamar a la función para ajustar el estado visual
-          chechFacebook();
-      });
   });
-
-
-  $(document).ready(function() {
-    // Evento de clic en el texto asociado a Facebook
-    $("#textFacebook").click(function() {
-        // Verificar el estado actual del switch
-        if (!$("#swFacebook").prop("checked")) {
-            // Cambiar el estado del switch solo si está apagado
-            $("#swFacebook").prop("checked", true);
-            // Llamar a la función para ajustar el estado visual
-            chechFacebook();
-        }
-    });
 });
 
-
-$(document).ready(function() {
+$(document).ready(function () {
   // Evento de clic en la tarjeta de instagram
   // Evento de clic en el texto asociado a Facebook
-  $("#textInstagram").click(function() {
-      // Verificar el estado actual del switch
-      if (!$("#swInstagram").prop("checked")) {
-          // Cambiar el estado del switch solo si está apagado
-          $("#swInstagram").prop("checked", true);
-          // Llamar a la función para ajustar el estado visual
-          chechInstagram();
-      }
+  $("#textInstagram").click(function () {
+    // Verificar el estado actual del switch
+    if (!$("#swInstagram").prop("checked")) {
+      // Cambiar el estado del switch solo si está apagado
+      $("#swInstagram").prop("checked", true);
+      // Llamar a la función para ajustar el estado visual
+      chechInstagram();
+    }
   });
 });
-
 
 //whatssap
-$(document).ready(function() {
+$(document).ready(function () {
   // Evento de clic en el texto asociado a Facebook
-  $("#textWhatsapp").click(function() {
-      // Verificar el estado actual del switch
-      if (!$("#swWhatsapp").prop("checked")) {
-          // Cambiar el estado del switch solo si está apagado
-          $("#swWhatsapp").prop("checked", true);
-          // Llamar a la función para ajustar el estado visual
-          chechWhatsApp();
-      }
+  $("#textWhatsapp").click(function () {
+    // Verificar el estado actual del switch
+    if (!$("#swWhatsapp").prop("checked")) {
+      // Cambiar el estado del switch solo si está apagado
+      $("#swWhatsapp").prop("checked", true);
+      // Llamar a la función para ajustar el estado visual
+      chechWhatsApp();
+    }
   });
 });
-
 
 //mensaje
-$(document).ready(function() {
+$(document).ready(function () {
   // Evento de clic en el texto asociado a Facebook
-  $("#textMensaje").click(function() {
-      // Verificar el estado actual del switch
-      if (!$("#swMensaje").prop("checked")) {
-          // Cambiar el estado del switch solo si está apagado
-          $("#swMensaje").prop("checked", true);
-          // Llamar a la función para ajustar el estado visual
-          chechMensajes();
-      }
+  $("#textMensaje").click(function () {
+    // Verificar el estado actual del switch
+    if (!$("#swMensaje").prop("checked")) {
+      // Cambiar el estado del switch solo si está apagado
+      $("#swMensaje").prop("checked", true);
+      // Llamar a la función para ajustar el estado visual
+      chechMensajes();
+    }
   });
 });
-
 
 //pantalla
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Evento de clic en el texto asociado a Facebook
-  $("#textPantalla").click(function() {
-      // Verificar el estado actual del switch
-      if (!$("#swPantalla").prop("checked")) {
-          // Cambiar el estado del switch solo si está apagado
-          $("#swPantalla").prop("checked", true);
-          // Llamar a la función para ajustar el estado visual
-          chechPantalla();
-      }
+  $("#textPantalla").click(function () {
+    // Verificar el estado actual del switch
+    if (!$("#swPantalla").prop("checked")) {
+      // Cambiar el estado del switch solo si está apagado
+      $("#swPantalla").prop("checked", true);
+      // Llamar a la función para ajustar el estado visual
+      chechPantalla();
+    }
   });
 });
 
-
-
-
+const Alert = function (
+  message,
+  status // si se proceso correctamente la solicitud
+) {
+  toastr[`${status}`](message, `${status}`, {
+    closeButton: true,
+    tapToDismiss: false,
+    positionClass: "toast-top-right",
+    rtl: false,
+  });
+};
