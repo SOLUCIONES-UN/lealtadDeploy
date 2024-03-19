54ce7911e21e0d1e71b8fde729b7919fb2127ff8
Anthony <arredondothony@gmail.com>
2024-03-19 14:43:15 -0600

    graficas, clientes y beneficios esperados.

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,17 +1,42 @@
 let usuarioDashboardDashboard = JSON.parse(localStorage.getItem("infousuarioDashboard"));
 
-$(function () {
-    getAllPromocionesActivas();
-    getAllCampanasActivas();
-    getAllCampanasActivasLastWeek();
-    getAllPromocionesActivasLastWeek();
+  
+const token = localStorage.getItem("token");
 
-    $("#ver-detalles-btn").click(function () {
-        //$("#graficaModal").modal("show");
-        var myModal = new bootstrap.Modal(document.getElementById("graficaModal"));
-        mostrarGraficaCampañas();
-        myModal.show();
-    });
+$(function () {
+  $('#datosGrafica').hide();
+  $('#datosGrafica1').hide();
+  $('#datosGrafica2').hide();
+  getAllCountCoustomeName();
+  getAllSumValor();
+  getAllCampanasActivasLastWeek();
+  getAllPromocionesActivasLastWeek();
+  getAllPromocionesActivas();
+  getAllCampanasActivas();
+});
+
+//boton para volver a pagina anterior
+$("#btnRegresar").click(function () {
+  $('#datosGrafica').hide();
+  $('#datosGrafica1').hide();
+  $('#datosGrafica2').hide();
+
+  $("#dataDashboard").show('low');
+
+})
+
+$("#ver-detalles-btn").click(function () {
+  // var myModal = new bootstrap.Modal(document.getElementById("graficaModal"));
+  // mostrarGraficaCampañas();
+
+  $('#dataDashboard').hide();
+  mostrarGraficaCampañas();
+  $('#datosGrafica').show();
+  mostrarGraficaCampañas1();
+  $('#datosGrafica1').show();
+  mostrarGraficaCampañas2();
+  $('#datosGrafica2').show();
+  // myModal.show();
 });
 
 
@@ -44,7 +69,7 @@ function mostrarGraficaCampañas() {
         datasets: [
           {
             label: "Número de clientes",
-            data: [20,10,40,33],
+            data: [20, 10, 40, 33],
             backgroundColor: "rgba(54, 162, 235, 0.2)",
             borderColor: "rgba(54, 162, 235, 1)",
             borderWidth: 1,
@@ -66,11 +91,150 @@ function mostrarGraficaCampañas() {
         data: chartData,
         options: chartOptions,
       });
+
+
     })
     .catch((error) =>
       console.error("Error al obtener datos de campañas:", error)
     );
 }
+
+function mostrarGraficaCampañas1() {
+  // Obtener el canvas
+  const canvas = document.getElementById("graficaCampanas1");
+    var labels = [];
+  for (var i = 1; i <= 12; i++) {
+      labels.push('Columna ' + i);
+  }
+  var data = [];
+for (var i = 0; i < 12; i++) {
+    data.push(Math.floor(Math.random() * 100)); // Generar valores aleatorios para cada columna
+}
+
+
+  const token = localStorage.getItem("token");
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+
+  fetch(`${url}Campania`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Datos de campañas:", data);
+
+      const labels = data.map((campaña) => campaña.nombre);
+      const numCampanas = data.map((campaña) => campaña.numero_campana);
+
+      const chartData = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Número de Campanas",
+            data: [20, 10, 40, 33, 22, 32, 23, 53, 55, 66, 33, 44],
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      const chartOptions = {
+        scales: {
+          yAxes: [{}],
+        },
+      };
+      console.log("Labels:", labels);
+      console.log("NumCampanas:", numCampanas);
+
+      const ctx = canvas.getContext("2d");
+      new Chart(ctx, {
+        type: "bar",
+        data: chartData,
+        options: chartOptions,
+      });
+
+
+    })
+    .catch((error) =>
+      console.error("Error al obtener datos de campañas:", error)
+    );
+}
+
+function mostrarGraficaCampañas2() {
+  // Obtener el canva
+  const canvas = document.getElementById("graficaCampanas2");
+    var labels = [];
+  for (var i = 1; i <= 12; i++) {
+      labels.push('Columna ' + i);
+  }
+  var data = [];
+for (var i = 0; i < 12; i++) {
+    data.push(Math.floor(Math.random() * 100)); // Generar valores aleatorios para cada columna
+}
+
+
+  const token = localStorage.getItem("token");
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+
+  fetch(`${url}Campania`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Datos de campañas:", data);
+
+      const labels = data.map((campaña) => campaña.nombre);
+      const numCampanas = data.map((campaña) => campaña.numero_campana);
+
+      const chartData = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Número de Campanas",
+            data: [20, 10, 40, 33, 22, 32, 23,],
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      const chartOptions = {
+        scales: {
+          yAxes: [{}],
+        },
+      };
+      console.log("Labels:", labels);
+      console.log("NumCampanas:", numCampanas);
+
+      const ctx = canvas.getContext("2d");
+      new Chart(ctx, {
+        type: "bar",
+        data: chartData,
+        options: chartOptions,
+      });
+
+
+    })
+    .catch((error) =>
+      console.error("Error al obtener datos de campañas:", error)
+    );
+};
+
 
 
 const cerrarModalBtn = document.getElementById("cerrar-modal-btn");
@@ -83,12 +247,12 @@ cerrarModalBtn.addEventListener("click", () => {
   modalInstance.hide();
 });
 
-const displayNumCampanas = (numCampanas) => {
+function displayNumCampanas(numCampanas) {
   const numCampanasElement = document.getElementById("num-campanas");
   numCampanasElement.textContent = numCampanas;
 };
 
-const getAllCampanasActivas = () => {
+function getAllCampanasActivas() {
   const token = localStorage.getItem("token");
 
   const headers = {
@@ -112,13 +276,13 @@ const getAllCampanasActivas = () => {
     .catch((error) => console.log("error", error));
 };
 
-const displayNumReferidos = (numReferidos) => {
+function displayNumReferidos(numReferidos) {
   const numReferidosElement = document.getElementById("num-referidos");
   numReferidosElement.textContent = numReferidos;
 };
 
 // Función para obtener y mostrar todas las participaciones activas
-const getAllParticipaciones = () => {
+function getAllParticipaciones() {
   const headers = {
     Authorization: `Bearer ${token}`,
     "Content-Type": "application/json",
@@ -138,12 +302,57 @@ const getAllParticipaciones = () => {
     .catch((error) => console.log("Error al obtener participaciones:", error));
 };
 
-const displayNumPromociones = (numPromociones) => {
+
+function getAllCountCoustomeName() {
+  console.log('\n\n\n\n\n\n Estoy dentro de la funcion Count \n\n\n\n')
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
+
+
+  fetch(`${url}Participacion/count`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Data de count:", result);
+      $('#clientesCount').text(result.totalParticipacions);
+    })
+    .catch((error) => console.log("Error al obtener la cantidad de clientes:", error));
+};
+
+function getAllSumValor() {
+  console.log('\n\n\n\n\n\n Estoy dentro de la funcion Sum \n\n\n\n')
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
+
+
+  fetch(`${url}Participacion/sumarvalor`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Data de count:", result);
+      $('#sumaValor').text("Q "+parseFloat(result.total).toFixed(2));
+    })
+    .catch((error) => console.log("Error al obtener el total de beneficios:", error));
+};
+
+function displayNumPromociones(numPromociones) {
   const numPromocionesElement = document.getElementById("num-promociones");
   numPromocionesElement.textContent = numPromociones;
 };
 
-const getAllPromocionesActivas = () => {
+function getAllPromocionesActivas() {
   const token = localStorage.getItem("token");
 
   const headers = {
@@ -157,6 +366,7 @@ const getAllPromocionesActivas = () => {
     redirect: "follow",
   };
 
+
   fetch(`${url}Promocion`, requestOptions)
     .then((response) => response.json())
     .then((result) => {
@@ -169,12 +379,12 @@ const getAllPromocionesActivas = () => {
     .catch((error) => console.log("error", error));
 };
 
-const displayNumCampanasLastWeek = (numCampanas) => {
+function displayNumCampanasLastWeek(numCampanas) {
   const numCampanasElement = document.getElementById("num-campanas-last-week");
   numCampanasElement.textContent = numCampanas;
 };
 
-const getAllCampanasActivasLastWeek = () => {
+function getAllCampanasActivasLastWeek() {
   const token = localStorage.getItem("token");
   const today = new Date();
   const lastWeekStart = new Date(
@@ -215,17 +425,17 @@ const getAllCampanasActivasLastWeek = () => {
       displayNumCampanasLastWeek(campanasActivasLastWeek.length);
     })
     .catch((error) => console.log("error", error));
-};
+}
 
 
-const displayNumPromocionesLastWeek = (numPromociones) => {
+function displayNumPromocionesLastWeek(numPromociones) {
   const numPromocionesElement = document.getElementById(
     "num-promociones-last-week"
   );
   numPromocionesElement.textContent = numPromociones;
 };
 
-const getAllPromocionesActivasLastWeek = () => {
+function getAllPromocionesActivasLastWeek() {
   const token = localStorage.getItem("token");
   const today = new Date();
   const lastWeekStart = new Date(
@@ -268,3 +478,4 @@ const getAllPromocionesActivasLastWeek = () => {
     .catch((error) => console.log("error", error));
 };
 
+   
