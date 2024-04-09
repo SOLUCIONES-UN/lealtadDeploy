83f710bbf7be898ef7f303abe95e0c15d0983cad
kanuso <kenedyhtml@gmail.com>
2024-04-09 10:33:25 -0600

    mejoras en Reporte

--- js/reportGeneral.js
+++ js/reportGeneral.js
@@ -1,17 +1,16 @@
 const url = "http://localhost:3000/";
 let token = localStorage.getItem("token");
-$(function () {
+$(function() {
 
-    $("#ConsultarReferido" ).click(function() {
-        if ($("#FechaInicio").val() !=="" && $("#FechaFin").val() !=="") {
+    $("#ConsultarReferido").click(function() {
+        if ($("#FechaInicio").val() !== "" && $("#FechaFin").val() !== "") {
             GetReport();
-        }else{
+        } else {
             Alert("Fecha inicial y fecha final es requerida, por favor vuelve a intentarlo.", "error");
         }
-        
-    });
-  });
 
+    });
+});
 
 
 const getParticipacionesFechasGeneral = (fecha1, fecha2) => {
@@ -25,48 +24,47 @@ const getParticipacionesFechasGeneral = (fecha1, fecha2) => {
 
     // Hacemos la solicitud al backend
     fetch(`${url}reporteGeneralReferido/${fecha1}/${fecha2}`, requestOptions)
-        .then((response) => {
+        .then(response => {
             if (!response.ok) {
                 throw new Error('Error al obtener los datos.');
             }
             return response.json();
         })
+        .then(datos => {
+            // Creamos un conjunto para almacenar los identificadores únicos
+            const referidosUnicos = new Set();
+            let datosUnicos = [];
 
+            // Combinamos los dos arrays en uno solo
+            const datosCombinados = [...datos[0], ...datos[1]];
 
-        .then((datos) => {
-            // Agregar la campaña obtenida a la tabla
-            const row = `
-                <tr>
-                    <td>${datos.fecha}</td>
-                    <td>${datos.codigo}</td>
-                </tr>`;
-            $("#TablaReporteReferidos").html(row); // Reemplazar el contenido de la tabla con el nuevo registro
+            // Filtramos los datos para eliminar duplicados
+            datosCombinados.forEach(dato => {
+                if (!referidosUnicos.has(dato.noReferido)) {
+                    referidosUnicos.add(dato.noReferido);
+                    datosUnicos.push(dato);
+                }
+            });
+
+            // Aquí puedes procesar los datos únicos obtenidos
+            let tabla = '';
+            datosUnicos.forEach((dato, index) => {
+                tabla += `
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${dato.fecha}</td>
+                        <td>${dato.codigo}</td>
+                        <td>Descripción</td> <!-- Reemplaza esto con la descripción correspondiente -->
+                        <td>${dato.nombreReferido}</td>
+                        <td>${dato.userno}</td>
+                        <td>${dato.noReferido}</td>
+                    </tr>
+                `;
+            });
+            $("#TablaReporteReferidos").html(tabla);
         })
-
-
-        // .then((datos) => {
-        //     // Aquí puedes procesar los datos obtenidos
-        //     let tabla = '';
-        //     datos.forEach((dato, index) => {
-        //         tabla += `
-        //             <tr>
-        //                 <td>${index + 1}</td>
-        //                 <td>${dato.fecha}</td>
-        //                 <td>${dato.codigo}</td>
-        //                 <td>Descripción</td> <!-- Reemplaza esto con la descripción correspondiente -->
-        //                 <td>${dato.nombreReferido}</td>
-        //                 <td>${dato.userno}</td>
-        //                 <td>${dato.noReferido}</td>
-        //             </tr>
-        //         `;
-        //     });
-        //     $("#TablaReporteReferidos").html(tabla);
-        // })
-
-        
-        .catch((error) => {
+        .catch(error => {
             console.error('Error en la solicitud:', error);
-            // Manejar el error si ocurre
             Alert('Ha ocurrido un error al obtener los datos.', 'error');
         });
 };
@@ -77,7 +75,6 @@ $("#ConsultarReferido").click(function() {
     if (fecha1 !== '' && fecha2 !== '') {
         getParticipacionesFechasGeneral(fecha1, fecha2);
     } else {
-        console.error('Por favor, ingrese fechas válidas.');
+        console.error('Por favor, ingresa ambas fechas.');
     }
 });
-
