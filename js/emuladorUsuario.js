a467c780b505d46d8e6fdf7d785287471a286d8d
kenedy <kenedyhtml@gmail.com>
2024-04-08 23:48:57 -0600

    se creo la interface para reportes Generales, funcionales

--- js/emuladorUsuario.js
+++ js/emuladorUsuario.js
@@ -23,65 +23,61 @@ $(function() {
 
 
 
+const campanasUsuariosEmulador_get = (telefono) => {
+    // Definimos los datos de la solicitud
+    var requestOptions = {
+        method: "GET",
+        headers: {
+            "Authorization": token
+        }
+    };
 
-// const GetNumeroById = (telno) => {
-//   // Definimos los datos de la solicitud
-//   var requestOptions = {
-//       method: "GET",
-//       headers: {
-//           "Authorization": token
-//       }
-//   };
+    // Hacemos la solicitud al backend
+    fetch(`${url}ConsultaNumber/${telefono}`, requestOptions)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Error al obtener el numero.');
+            }
+            return response.json();
+        })
+        .then((numero) => {
+            // Agregar la campaña obtenida a la tabla
+            const row = `
+                <tr>
+                    <td>${numero.id}</td>
+                    <td>${numero.campaign.nombre}</td>
+                </tr>`;
+            $("#TablaReportePromo").html(row); // Reemplazar el contenido de la tabla con el nuevo registro
+        })
+        .catch((error) => {
+            console.error('Error en la solicitud:', error);
+            // Manejar el error si ocurre
+            Alert('Ha ocurrido un error al obtener el número.', 'error');
+        });
+};
 
-//   // Hacemos la solicitud al backend
-//   fetch(`${url}ConsultaNumber/${telno}`, requestOptions)
-//       .then((response) => {
-//           if (!response.ok) {
-//               throw new Error('Error al obtener el numero.');
-//           }
-//           return response.json();
-//       })
-//       .then((numero) => {
-//           // Agregar la campaña obtenida a la tabla
-//           const row = `
-//               <tr>
-//                   <td>${numero.id}</td>
-//                   <td>${numero.campaign.nombre}</td>
-//               </tr>`;
-//           $("#TablaReportePromo").html(row); // Reemplazar el contenido de la tabla con el nuevo registro
-//       })
-//       .catch((error) => {
-//           console.error('Error en la solicitud:', error);
-//           // Manejar el error si ocurre
-//           Alert('Ha ocurrido un error al obtener el número.', 'error');
-//       });
-// };
-
-// const Alert = function (
-// message,
-// status // si se proceso correctamente la solicitud
-// ) {
-// toastr[`${status}`](message, `${status}`, {
-//   closeButton: true,
-//   tapToDismiss: false,
-//   positionClass: "toast-top-right",
-//   rtl: false,
-// });
-// };
-
-
-// $("#ConsultaNUmber").click(function() {
-//   const telno = $("#numero").val().trim();
-//   if (telno !== '') {
-//       GetNumeroById(telno);
-//   } else {
-//       console.error('Por favor, ingrese un número de teléfono válido.');
-
-//   }
-// });
+const Alert = function(
+    message,
+    status // si se proceso correctamente la solicitud
+) {
+    toastr[`${status}`](message, `${status}`, {
+        closeButton: true,
+        tapToDismiss: false,
+        positionClass: "toast-top-right",
+        rtl: false,
+    });
+};
 
 
+$("#ConsultaNUmber").click(function() {
+    const telefono = $("#numero").val().trim();
+    if (telefono !== '') {
+        campanasUsuariosEmulador_get(telefono);
+    } else {
+        console.error('Por favor, ingrese un número de teléfono válido.');
 
+    }
+});
 
 
 
@@ -160,65 +156,65 @@ $(function() {
 
 
 
-const generaCampanasUsuarios = (referens) => {
-    // Definimos los datos de la solicitud
-    var requestOptions = {
-        method: "GET",
-        headers: {
-            "Authorization": token
-        }
-    };
+// const generaCampanasUsuarios = (referens) => {
+//     // Definimos los datos de la solicitud
+//     var requestOptions = {
+//         method: "GET",
+//         headers: {
+//             "Authorization": token
+//         }
+//     };
 
-    // Hacemos la solicitud al backend
-    fetch(`${url}ConsultaNumber/${referens}`, requestOptions)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Error al obtener el numero.');
-            }
-            return response.json();
-        })
-        .then((campanas) => {
-            let rows = '';
-            campanas.forEach((campana) => {
-                rows += `
-                  <tr>
-                      <td>${campana.id}</td>
-                      <td>${campana.nombre}</td>
-                      <!-- Agrega más celdas según sea necesario para otros atributos -->
-                  </tr>`;
-            });
-            $("#TablaReportePromo").html(rows); // Reemplazar el contenido de la tabla con las filas generadas
-        })
-        .catch((error) => {
-            console.error('Error en la solicitud:', error);
-            // Manejar el error si ocurre
-            Alert('Ha ocurrido un error al obtener las campañas.', 'error');
-        });
-};
-
-
+//     // Hacemos la solicitud al backend
+//     fetch(`${url}ConsultaNumber/${referens}`, requestOptions)
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error('Error al obtener el numero.');
+//             }
+//             return response.json();
+//         })
+//         .then((campanas) => {
+//             let rows = '';
+//             campanas.forEach((campana) => {
+//                 rows += `
+//                   <tr>
+//                       <td>${campana.id}</td>
+//                       <td>${campana.nombre}</td>
+//                       <!-- Agrega más celdas según sea necesario para otros atributos -->
+//                   </tr>`;
+//             });
+//             $("#TablaReportePromo").html(rows); // Reemplazar el contenido de la tabla con las filas generadas
+//         })
+//         .catch((error) => {
+//             console.error('Error en la solicitud:', error);
+//             // Manejar el error si ocurre
+//             Alert('Ha ocurrido un error al obtener las campañas.', 'error');
+//         });
+// };
 
 
 
-const Alert = function(
-    message,
-    status // si se proceso correctamente la solicitud
-) {
-    toastr[`${status}`](message, `${status}`, {
-        closeButton: true,
-        tapToDismiss: false,
-        positionClass: "toast-top-right",
-        rtl: false,
-    });
-};
 
 
-$("#ConsultaNUmber").click(function() {
-    const referens = $("#numero").val().trim();
-    if (referens !== '') {
-        generaCampanasUsuarios(referens);
-    } else {
-        console.error('Por favor, ingrese un número de teléfono válido.');
+// const Alert = function(
+//     message,
+//     status // si se proceso correctamente la solicitud
+// ) {
+//     toastr[`${status}`](message, `${status}`, {
+//         closeButton: true,
+//         tapToDismiss: false,
+//         positionClass: "toast-top-right",
+//         rtl: false,
+//     });
+// };
 
-    }
-});
+
+// $("#ConsultaNUmber").click(function() {
+//     const referens = $("#numero").val().trim();
+//     if (referens !== '') {
+//         generaCampanasUsuarios(referens);
+//     } else {
+//         console.error('Por favor, ingrese un número de teléfono válido.');
+
+//     }
+// });
