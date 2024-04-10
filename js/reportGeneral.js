49ae54fff5232cbc74a01a346b43bc6b70d8474a
kenedy <kenedyhtml@gmail.com>
2024-04-10 14:18:09 -0600

    mejoras en el excel

--- js/reportGeneral.js
+++ js/reportGeneral.js
@@ -33,12 +33,10 @@ const getParticipacionesFechasGeneral = (fecha1, fecha2) => {
             return response.json();
         })
         .then(datos => {
-            // Mostrar los botones y la tabla
             $("#MostrarTabla").show();
             $("#excel").show();
             $(".card-datatable").show();
 
-            // Mostrar la tabla solo después de hacer clic en el botón "Mostrar Tabla"
             $("#MostrarTabla").click(function() {
                 mostrarDatosEnTabla(datos);
             });
@@ -54,7 +52,6 @@ const getParticipacionesFechasGeneral = (fecha1, fecha2) => {
 
 const mostrarDatosEnTabla = (datos) => {
     let tabla = '';
-    // Iteramos sobre cada conjunto de datos recibidos
     datos.forEach(array => {
         array.forEach(dato => {
             tabla += `
@@ -69,41 +66,90 @@ const mostrarDatosEnTabla = (datos) => {
             `;
         });
     });
-    // Insertamos las filas en la tabla
     $("#TablaReporteReferidos").html(tabla);
 };
 
 
-
 const generarExcel = (datos) => {
-    // Crear una hoja de trabajo
     const wb = XLSX.utils.book_new();
-    // Convertir los datos de la tabla en una matriz
+
+    const headerStyle = {
+        font: { sz: 14, bold: true, color: { rgb: "000000" } },
+        alignment: { horizontal: "center" },
+        fill: { fgColor: { rgb: "CCCCCC" } }
+    };
+
+    const columnTitleStyle = {
+        font: { sz: 12, bold: true, color: { rgb: "FFFFFF" } }, // Blanco
+        fill: { fgColor: { rgb: "808080" } }, // Gris
+        alignment: { horizontal: "center" }
+    };
+
+    const dataCellStyle = {
+        font: { sz: 11, color: { rgb: "000000" } },
+        border: {
+            top: { style: "thin", color: { rgb: "000000" } },
+            bottom: { style: "thin", color: { rgb: "000000" } },
+            left: { style: "thin", color: { rgb: "000000" } },
+            right: { style: "thin", color: { rgb: "000000" } }
+        }
+    };
+
+    const titles = ['               ','NUMERO DE TELÉFONO  ', 'NOMBRE DE REFERIDOR ', 'CÓDIGO REFERIDO ', 'NUMERO DE REFERIDO ', 'NOMBRE DE REFERIDO ', 'FECHA Y HORA   '];
+
     const data = [
-        ['Reporte General de Referidos'],
-        ['NUMERO DE TELÉFONO', 'Nombre Referidor', 'CÓDIGO REFERIDO', 'NUMERO DE REFERIDO', 'Nombre de referido', 'FECHA Y HORA']
+        [{ v: '                ', s: headerStyle }, { v: 'REPORTE GENERAL DE REFERIDOS', s: headerStyle }],
+        [],
+        [],
+        titles.map(title => ({ v: title, s: columnTitleStyle }))
     ];
-    datos.forEach(array => {
+
+    datos.forEach((array, index) => {
         array.forEach(dato => {
-            data.push([dato.userno, dato.nombreReferidor, dato.codigo, dato.noReferido, dato.nombreReferido, dato.fecha]);
+            data.push([
+                { v: ' ', s: dataCellStyle },
+                { v: dato.codigo, s: dataCellStyle },
+                { v: dato.nombreReferidor, s: dataCellStyle },
+                { v: dato.userno, s: dataCellStyle },
+                { v: dato.noReferido, s: dataCellStyle },
+                { v: dato.nombreReferido, s: dataCellStyle },
+                { v: dato.fecha, s: dataCellStyle }
+            ]);
         });
     });
-    // Crear una hoja de cálculo
+
     const ws = XLSX.utils.aoa_to_sheet(data);
-    // Agregar la hoja de cálculo al libro de trabajo
+
+    const titleWidths = titles.map(title => title.length);
+
+    ws['!cols'] = titleWidths.map(w => ({ wch: w }));
+
     XLSX.utils.book_append_sheet(wb, ws, 'Reporte Referidos');
-    // Convertir el libro de trabajo a un archivo de Excel
+
     const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-    // Descargar el archivo Excel
+
     saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }), 'Reporte_Referidos.xlsx');
 };
-// Función auxiliar para convertir datos en binario
+
 const s2ab = (s) => {
     const buf = new ArrayBuffer(s.length);
     const view = new Uint8Array(buf);
     for (let i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
     return buf;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -129,187 +175,3 @@ $("#MostrarTabla").click(function() {
 
 
 
-
-
-
-
-
-// $(function() {
-//     $("#ConsultarReferido").click(function() {
-//         const fecha1 = $("#fecha1").val().trim();
-//         const fecha2 = $("#fecha2").val().trim();
-//         if (fecha1 !== '' && fecha2 !== '') {
-//             getParticipacionesFechasGeneral(fecha1, fecha2);
-//         } else {
-//             console.error('Por favor, ingresa ambas fechas.');
-//         }
-//     });
-// });
-
-// const getParticipacionesFechasGeneral = (fecha1, fecha2) => {
-//     var requestOptions = {
-//         method: "GET",
-//         headers: {
-//             "Authorization": token
-//         }
-//     };
-
-//     fetch(`${url}reporteGeneralReferido/${fecha1}/${fecha2}`, requestOptions)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Error al obtener los datos.');
-//             }
-//             return response.json();
-//         })
-//         .then(datos => {
-//             mostrarDatosEnTabla(datos);
-
-//             // Mostrar los botones y la tabla
-//             $("#MostrarTabla").show();
-//             $("#excel").show();
-//             $(".card-datatable").show();
-//         })
-//         .catch(error => {
-//             console.error('Error en la solicitud:', error);
-//             Alert('Ha ocurrido un error al obtener los datos.', 'error');
-//         });
-// };
-
-// $("#MostrarTabla").click(function() {
-//     const fecha1 = $("#fecha1").val().trim();
-//     const fecha2 = $("#fecha2").val().trim();
-//     if (fecha1 !== '' && fecha2 !== '') {
-//         getParticipacionesFechasGeneral(fecha1, fecha2);
-//     } else {
-//         console.error('Por favor, ingresa ambas fechas.');
-//     }
-// });
-
-// const mostrarDatosEnTabla = (datos) => {
-//     let tabla = '';
-//     // Iteramos sobre cada conjunto de datos recibidos
-//     datos.forEach(array => {
-//         array.forEach(dato => {
-//             tabla += `
-//                 <tr>
-//                     <td>${dato.userno}</td>
-//                     <td>${dato.nombreReferidor}</td>
-//                     <td>${dato.codigo}</td>
-//                     <td>${dato.noReferido}</td>
-//                     <td>${dato.nombreReferido}</td>
-//                     <td>${dato.fecha}</td>
-//                 </tr>
-//             `;
-//         });
-//     });
-//     // Insertamos las filas en la tabla
-//     $("#TablaReporteReferidos").html(tabla);
-
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(function() {
-
-//     $("#ConsultarReferido").click(function() {
-//         if ($("#FechaInicio").val() !== "" && $("#FechaFin").val() !== "") {
-//             GetReport();
-//         } else {
-//             Alert("Fecha inicial y fecha final es requerida, por favor vuelve a intentarlo.", "error");
-//         }
-
-//     });
-// });
-
-
-// const getParticipacionesFechasGeneral = (fecha1, fecha2) => {
-//     // Definimos los datos de la solicitud
-//     var requestOptions = {
-//         method: "GET",
-//         headers: {
-//             "Authorization": token
-//         }
-//     };
-
-//     // Hacemos la solicitud al backend
-//     fetch(`${url}reporteGeneralReferido/${fecha1}/${fecha2}`, requestOptions)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Error al obtener los datos.');
-//             }
-//             return response.json();
-//         })
-//         .then(datos => {
-//             // Creamos un conjunto para almacenar los identificadores únicos
-//             const referidosUnicos = new Set();
-//             let datosUnicos = [];
-
-//             // Combinamos los dos arrays en uno solo
-//             const datosCombinados = [...datos[0], ...datos[1]];
-
-//             // Filtramos los datos para eliminar duplicados
-//             datosCombinados.forEach(dato => {
-//                 if (!referidosUnicos.has(dato.noReferido)) {
-//                     referidosUnicos.add(dato.noReferido);
-//                     datosUnicos.push(dato);
-//                 }
-//             });
-
-//             // Aquí puedes procesar los datos únicos obtenidos
-//             let tabla = '';
-//             datosUnicos.forEach((dato, index) => {
-//                 tabla += `
-//                     <tr>
-//                         <td>${index + 1}</td>
-//                         <td>${dato.fecha}</td>
-//                         <td>${dato.codigo}</td>
-//                         <td>Descripción</td> <!-- Reemplaza esto con la descripción correspondiente -->
-//                         <td>${dato.nombreReferido}</td>
-//                         <td>${dato.userno}</td>
-//                         <td>${dato.noReferido}</td>
-//                     </tr>
-//                 `;
-//             });
-//             $("#TablaReporteReferidos").html(tabla);
-//         })
-//         .catch(error => {
-//             console.error('Error en la solicitud:', error);
-//             Alert('Ha ocurrido un error al obtener los datos.', 'error');
-//         });
-// };
-
-// $("#ConsultarReferido").click(function() {
-//     const fecha1 = $("#fecha1").val().trim();
-//     const fecha2 = $("#fecha2").val().trim();
-//     if (fecha1 !== '' && fecha2 !== '') {
-//         getParticipacionesFechasGeneral(fecha1, fecha2);
-//     } else {
-//         console.error('Por favor, ingresa ambas fechas.');
-//     }
-// });
