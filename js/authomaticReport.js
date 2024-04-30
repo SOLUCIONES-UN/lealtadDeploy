4ff3b7a40302afe502a0ea7c3a5d6cf21a98df79
kanuso <kenedyhtml@gmail.com>
2024-04-30 16:15:25 -0600

    cambios y mejoras  ccc

--- js/authomaticReport.js
+++ js/authomaticReport.js
@@ -13,8 +13,8 @@ $(function() {
         placeholder: "Elige una promoción",
     });
 
-     // Eventos para el modal New
-     $('#modalNew').on('hidden.bs.modal', function () {
+    // Eventos para el modal New
+    $('#modalNew').on('hidden.bs.modal', function() {
         limpiarFormulario();
         $("#btnSubmit").attr("disabled", false);
     });
@@ -22,11 +22,11 @@ $(function() {
 
 
 
- // Evento para el botón de cerrar el modal New
- $('#modalNew').find('[data-dismiss="modal"]').click(function () {
-    limpiarFormulario();
-    $("#btnSubmit").attr("disabled", false);
-});
+    // Evento para el botón de cerrar el modal New
+    $('#modalNew').find('[data-dismiss="modal"]').click(function() {
+        limpiarFormulario();
+        $("#btnSubmit").attr("disabled", false);
+    });
 
 
     $("#ConsultarPromo").on("click", function() {
@@ -93,6 +93,61 @@ const getCampanias = () => {
             alert(error, "error");
         });
 };
+
+
+function mostrarOcultarSelectDia() {
+    var selectConfiguracion = document.getElementById("seleccionConfiguracion");
+    var selectDiaContainer = document.getElementById("selectDiaContainer");
+
+    if (selectConfiguracion.value === "semana") {
+        selectDiaContainer.style.display = "block";
+    } else {
+        selectDiaContainer.style.display = "none";
+    }
+}
+
+
+
+function mostrarOcultarSelectDiaMes() {
+    var selectConfiguracion = document.getElementById("seleccionConfiguracion");
+    var selectDiaMesContainer = document.getElementById("selectDiaMesContainer");
+    var selectDiaMes = document.getElementById("selectDiaMes");
+
+    if (selectConfiguracion.value === "mes") {
+        selectDiaMesContainer.style.display = "block";
+        // Limpiar el select antes de agregar opciones
+        selectDiaMes.innerHTML = "";
+        // Agregar opciones del 1 al 30
+        for (var i = 1; i <= 30; i++) {
+            var option = document.createElement("option");
+            option.value = i;
+            option.text = i;
+            selectDiaMes.appendChild(option);
+        }
+    } else {
+        selectDiaMesContainer.style.display = "none";
+    }
+}
+
+$(function() {
+    // Otro código que puedas tener...
+
+    // Llama a la función mostrarOcultarSelectDiaMes cuando cambie el valor del primer select
+    $('#seleccionConfiguracion').change(function() {
+        mostrarOcultarSelectDiaMes();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 const getReport = () => {
     var myHeaders = new Headers();
@@ -179,117 +234,18 @@ function mostrarDatosEnTabla(datos) {
 
         },
         scrollX: true,
-        buttons: [
-            {
-                text: 'Nuevo',
-                className: 'add-new btn btn-primary mt-50',
-                attr: {
-                    'data-toggle': 'modal',
-                    'data-target': '#modalNew',
-                },
-                init: function (api, node, config) {
-                    $(node).removeClass('btn-secondary');
-                    //Metodo para agregar un nuevo usuario
-                },
+        buttons: [{
+            text: 'Nuevo',
+            className: 'add-new btn btn-primary mt-50',
+            attr: {
+                'data-toggle': 'modal',
+                'data-target': '#modalNew',
             },
-        ],
+            init: function(api, node, config) {
+                $(node).removeClass('btn-secondary');
+                //Metodo para agregar un nuevo usuario
+            },
+        }, ],
     });
-    
+
 }
-
-
-document.getElementById("btnDescargarExcel").addEventListener("click", function() {
-    console.log("Descargar Excel");
-
-    const table = document.getElementById("TablaReportePromo"); // Obtener la tabla
-    const wb = XLSX.utils.book_new(); // Crear un nuevo libro de Excel
-
-    // Obtener los datos de la tabla
-    const data = [];
-    let lineNumber = 1;
-
-    for (let i = 0; i < table.rows.length; i++) {
-        const row = [];
-        row.push({ v: lineNumber++ });
-        for (let j = 0; j < table.rows[i].cells.length; j++) {
-            row.push(table.rows[i].cells[j].innerText);
-        }
-        // Insertar una celda vacía al principio del array para iniciar desde la columna "A"
-        row.unshift("");
-        data.push(row);
-    }
-
-    // Agregar el encabezado
-    const headerRow1 = [
-        { v: '', t: 's', s: { font: { name: 'Courier', sz: 18 } } },
-        { v: '', t: 's', s: { font: { sz: 18 }, alignment: { horizontal: 'center' } } },
-        { v: '', t: 's', s: { font: { sz: 18 }, alignment: { horizontal: 'center' } } },
-        { v: '', t: 's', s: { font: { sz: 18 }, alignment: { horizontal: 'center' } } },
-        { v: ' REPORTE DE NOTIFICACIONES  OFFERCRAFT', t: 's', s: { font: { sz: 18 }, alignment: { horizontal: 'center' } } },
-    ];
-    const headerRow2 = [
-        { v: '', t: 's', s: { font: { name: 'Courier', sz: 12 } } },
-        { v: '', t: 's', s: { font: { sz: 12 }, alignment: { horizontal: 'center' } } },
-    ];
-    const headerRow3 = [''];
-    const headerRow4 = [
-        '',
-        { v: '#', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Fecha Acreditacion', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Telefono', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Nombre', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Campaña', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Premio', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Monto Premio', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Transaccion', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Codigo', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Monto Transaccion', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-        { v: 'Fecha Participacion', t: 's', s: { font: { bold: true, color: { rgb: 'FFFFFF' } }, alignment: { horizontal: 'center' }, fill: { fgColor: { rgb: '808080' } } } },
-    ];
-    data.unshift(headerRow1, headerRow2, headerRow3, headerRow4);
-
-    const ws = XLSX.utils.aoa_to_sheet(data);
-
-    // Ajustar el ancho de las columnas al contenido
-    ws['!cols'] = [
-        { wch: 15 },
-        { wch: 15 },
-        { wch: 12 },
-        { wch: 25 },
-        { wch: 25 }, // Ajuste de ancho para 'Campaña' y 'Fecha Participacion'
-        { wch: 20 },
-        { wch: 20 },
-        { wch: 12 },
-        { wch: 20 },
-        { wch: 12 },
-    ];
-
-    // Combinar las celdas E1, F1 y G1
-    if (!ws['!merges']) ws['!merges'] = [];
-    ws['!merges'].push({ s: { r: 0, c: 4 }, e: { r: 0, c: 6 } });
-
-    // Agregar la hoja al libro
-    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-
-    // Descargar el archivo Excel
-    XLSX.writeFile(wb, "reporte_OfferCraft.xlsx");
-});
-
-function formatearFechaHora(fechaHora) {
-    const fecha = new Date(fechaHora);
-    const dia = fecha.getDate().toString().padStart(2, "0");
-    const mes = (fecha.getMonth() + 1).toString().padStart(2, "0");
-    const año = fecha.getFullYear();
-    const horas = fecha.getHours().toString().padStart(2, "0");
-    const minutos = fecha.getMinutes().toString().padStart(2, "0");
-    return `${dia}/${mes}/${año}`;
-}
-
-const Alert = function(message, status) {
-    toastr[`${status}`](message, `${status}`, {
-        closeButton: true,
-        tapToDismiss: false,
-        positionClass: "toast-top-right",
-        rtl: false,
-    });
-};
