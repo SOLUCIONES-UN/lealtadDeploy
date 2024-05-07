4bb6cee69b3f158f06913eff56f7244388512e28
Anthony <arredondothony@gmail.com>
2024-05-07 14:44:22 -0600

    Funcionalidad Reporte y Grafica

--- js/graficaParticipantes.js
+++ js/graficaParticipantes.js
@@ -41,16 +41,30 @@ function getMeses(event) {
     .then((response) => response.json())
     .then((data) => {
       console.log("Datos de aios:", data);
-      let options = `<option value="0">MES</option>`;
-      data.forEach((d) => {
-        options += `<option value="${
-          d.mesValido < 10 ? "0" + d.mesValido : d.mesValido
-        }">${d.mesValido < 10 ? "0" + d.mesValido : d.mesValido}</option>`;
-      });
+      const meses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+    let options = `<option value="0">MES</option>`;
+    data.forEach((d) => {
+        const nombreMes = meses[d.mesValido - 1]; // Restamos 1 porque los índices de array comienzan en 0
+        options += `<option value="${d.mesValido}">${nombreMes}</option>`;
+    });
 
       $("#mesCampana").html(options);
     })
     .catch((error) => console.error("Error al obtener los anios:", error));
+}
+
+function llenarTabla(data) {
+  const listaDatos = $("#listaDatos");
+  listaDatos.html(''); // Limpiar contenido previo de la tabla
+
+  data.forEach((data) => {
+      const row = `<tr><td>${data.fechaRegistro}</td><td>${data.nombre}</td><td>${data.participantes}</td></tr>`;
+      listaDatos.append(row);
+  });
+  $("#container").hide();
 }
 $("#btnConsultar").click(function () {
   const headers = {
@@ -80,10 +94,14 @@ $("#btnConsultar").click(function () {
     )
       .then((response) => response.json())
       .then((data) => {
+        
         console.log("Datos de campañas:", data);
 
         // Obtener el canvas y su contexto
+        
         const canvas = document.getElementById("graficaParticipantes");
+        // canvas.width = 400; // Establecer el ancho del canvas
+        // canvas.height = 400
         const ctx = canvas.getContext("2d");
 
         // Destruir el gráfico existente si existe
@@ -92,17 +110,24 @@ $("#btnConsultar").click(function () {
         }
 
         // Obtener datos para el gráfico
-        const labels = data.map((c) => c.nombre);
-        const participantes = data.map((c) => c.participantes);
-
+        const dataLabels = data.map((p) => `${p.nombre} - ${p.fechaRegistro}`); // Combinar nombre y fecha como etiquetas
+        const participantes = data.map((p) => p.participantes);
+        
         // Configuración del gráfico
         const chartData = {
-          labels: labels,
+          labels: dataLabels,
           datasets: [
             {
               label: "Participantes",
               data: participantes,
-              backgroundColor: "rgba(117, 85, 245)",
+              backgroundColor: [
+                "rgba(41, 182, 246)",
+                "rgba(129, 199, 132)",
+                "rgba(21, 101, 192)",
+                "rgba(165, 105, 189 )",
+                "rgba(102, 102, 255)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
               borderColor: "rgba(54, 162, 235, 1)",
               borderWidth: 1,
             },
@@ -110,22 +135,88 @@ $("#btnConsultar").click(function () {
         };
 
         const chartOptions = {
+          maintainAspectRatio: true,
+          responsive: false,// Establecer en false para mantener el tamaño constante
           scales: {
             y: {
               beginAtZero: true, // Empezar el eje Y desde cero
             },
           },
         };
-
+        
         // Crear el gráfico
         window.myChart = new Chart(ctx, {
           type: "bar",
           data: chartData,
           options: chartOptions,
         });
+        
       })
       .catch((error) =>
         console.error("Error al obtener los datos de las campañas:", error)
       );
   }
 });
+let chartType = "bar"; // Variable para controlar el tipo de gráfico actual
+
+$("#btnGrafCircular").click(function () {
+
+  
+  if (window.myChart) {
+    // Destruir el gráfico existente
+    window.myChart.destroy();
+
+    // Cambiar el tipo de gráfico
+    chartType = chartType === "bar" ? "doughnut" : "bar";
+
+    // Obtener los datos para el nuevo gráfico
+    const chartData = window.myChart.config.data;
+    const labels = chartData.labels;
+    const data = chartData.datasets[0].data;
+
+    // Crear el nuevo gráfico con maintainAspectRatio establecido en false
+    const canvas = document.getElementById("graficaParticipantes");
+    const ctx = canvas.getContext("2d");
+    window.myChart = new Chart(ctx, {
+      type: chartType,
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Participantes",
+            data: data,
+            backgroundColor: [
+              "rgba(41, 182, 246)",
+              "rgba(129, 199, 132)",
+              "rgba(21, 101, 192)",
+              "rgba(165, 105, 189 )",
+              "rgba(102, 102, 255)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(253, 254, 254)",
+              "rgba(253, 254, 254)",
+              "rgba(253, 254, 254)",
+              "rgba(253, 254, 254)",
+              "rgba(253, 254, 254)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false, // Establecer en false para mantener el tamaño constante
+      },
+    });
+  }
+});
+// document.getElementById('btnGrafCircular').addEventListener('click', function() {
+//   var container = document.getElementById('container');
+//   if (container.style.display === 'none') {
+//       container.style.display = 'block'; // Mostrar el contenedor
+//   } else {
+//       container.style.display = 'none'; // Ocultar el contenedor
+//   }
+// });
