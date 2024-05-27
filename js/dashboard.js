f59c3b6a52f6921c2479093dc15c6d45ca3044dd
kenedy <kenedyhtml@gmail.com>
2024-05-27 12:07:53 -0600

    cambios en el dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,6 +8,8 @@ $(function () {
   
   getAllCountCoustomeName();
   getAllSumValor();
+  getTransaccion();
+  getReferidos();
   getAllCampanasActivasLastWeek();
   getAllPromocionesActivasLastWeek();
   getAllPromocionesActivas();
@@ -15,235 +17,7 @@ $(function () {
   // mostrarGraficaCampañas();
 });
 
-// //boton para volver a pagina anterior
-// $("#btnRegresar").click(function () {
-//   $('#datosGrafica').hide();
-  
-//   $("#dataDashboard").show('low');
 
-// })
-
-// $("#ver-detalles-btn").click(function () {
-//   // var myModal = new bootstrap.Modal(document.getElementById("graficaModal"));
-//   // mostrarGraficaCampañas();
-
-//   $('#dataDashboard').hide();
-
-//   $('#datosGrafica').show();
-// //   mostrarGraficaCampañas();
-// //   // mostrarGraficaCampañas1();
-// //   // $('#datosGrafica1').show();
-// //   // mostrarGraficaCampañas2();
-// //   // $('#datosGrafica2').show();
-// //   // myModal.show();
-// // });
-
-
-// function mostrarGraficaCampañas() {
-//   // Obtener el canvas
-  
-//   const canvas = document.getElementById("graficaCampanas");
-  
-//   const token = localStorage.getItem("token");
-//   // const token = localStorage.getItem("token");
-//   const headers = {
-//     Authorization: token,
-//     "Content-Type": "application/json",
-//   };
-
-//   var requestOptions = {
-//     method: "GET",
-//     headers: headers,
-    
-//     redirect: "follow",
-
-//   };
-
-//   fetch(`${url}Campania`, requestOptions)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       if (!data) {
-//         console.error("La respuesta de la solicitud fetch es nula");
-//         return;
-//       }
-//       console.log("Datos de campañas:", data);
-
-//       const labels = data.map((campana) => campana.nombre);
-//       const numClientes = data.map((campana) => campana.numero_Clientes);
-   
-//       const chartData = {
-//         labels: labels,
-//         datasets: [
-//           {
-//             label: "Número de clientes",
-//             data: numClientes,
-//             backgroundColor: "rgba(54, 162, 235, 0.2)",
-//             borderColor: "rgba(54, 162, 235, 1)",
-//             borderWidth: 1,
-//           },
-//         ],
-//       };
-//       const chartOptions = {
-//         scales: {
-//           yAxes: [{
-//             ticks: {
-//               beginAtZero: true // Esto asegura que la escala comience en cero
-//             }
-//           }]
-//         }
-//       };
-//       console.log("Labels:", labels);
-//       console.log("NumClientes:", numClientes);
-
-//       const ctx = canvas.getContext("2d");
-//       new Chart(ctx, {
-//         type: "bar",
-//         data: chartData,
-//         options: chartOptions,
-//       });
-
-
-//     })
-//     .catch((error) =>
-//       console.error("Error al obtener datos de campañas:", error)
-//     );
-// }
-
-// function mostrarGraficaCampañas1() {
-//   // Obtener el canvas
-//   const canvas = document.getElementById("graficaCampanas1");
-//   var labels = [];
-//   for (var i = 1; i <= 12; i++) {
-//     labels.push('Columna ' + i);
-//   }
-//   var data = [];
-//   for (var i = 0; i < 12; i++) {
-//     data.push(Math.floor(Math.random() * 100)); // Generar valores aleatorios para cada columna
-//   }
-
-
-//   const token = localStorage.getItem("token");
-//   const headers = {
-//     Authorization: token,
-//     "Content-Type": "application/json",
-//   };
-
-//   var requestOptions = {
-//     method: "GET",
-//     headers: headers,
-//     redirect: "follow",
-//   };
-
-//   fetch(`${url}Campania`, requestOptions)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log("Datos de campañas:", data);
-
-//       const labels = data.map((campaña) => campaña.nombre);
-//       const numCampanas = data.map((campaña) => campaña.numero_campana);
-
-//       const chartData = {
-//         labels: labels,
-//         datasets: [
-//           {
-//             label: "Número de Campanas",
-//             data: [20, 10, 40, 33, 22, 32, 23, 53, 55, 66, 33, 44],
-//             backgroundColor: "rgba(54, 162, 235, 0.2)",
-//             borderColor: "rgba(54, 162, 235, 1)",
-//             borderWidth: 1,
-//           },
-//         ],
-//       };
-
-//       const chartOptions = {
-//         scales: {
-//           yAxes: [{}],
-//         },
-//       };
-//       console.log("Labels:", labels);
-//       console.log("NumCampanas:", numCampanas);
-
-//       const ctx = canvas.getContext("2d");
-//       new Chart(ctx, {
-//         type: "bar",
-//         data: chartData,
-//         options: chartOptions,
-//       });
-
-
-//     })
-//     .catch((error) =>
-//       console.error("Error al obtener datos de campañas:", error)
-//     );
-// }
-
-// function mostrarGraficaCampañas2() {
-//   // Obtener el canva
-//   const canvas = document.getElementById("graficaCampanas2");
-//   var labels = [];
-//   for (var i = 1; i <= 12; i++) {
-//     labels.push('Columna ' + i);
-//   }
-//   var data = [];
-//   for (var i = 0; i < 12; i++) {
-//     data.push(Math.floor(Math.random() * 100)); // Generar valores aleatorios para cada columna
-//   }
-
-
-//   const token = localStorage.getItem("token");
-//   const headers = {
-//     Authorization: token,
-//     "Content-Type": "application/json",
-//   };
-
-//   var requestOptions = {
-//     method: "GET",
-//     headers: headers,
-//     redirect: "follow",
-//   };
-
-//   fetch(`${url}Campania`, requestOptions)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log("Datos de campañas:", data);
-
-//       const labels = data.map((campaña) => campaña.nombre);
-//       const numCampanas = data.map((campaña) => campaña.numero_campana);
-
-//       const chartData = {
-//         labels: labels,
-//         datasets: [
-//           {
-//             label: "Número de Campanas",
-//             data: [20, 10, 40, 33, 22, 32, 23,],
-//             backgroundColor: "rgba(54, 162, 235, 0.2)",
-//             borderColor: "rgba(54, 162, 235, 1)",
-//             borderWidth: 1,
-//           },
-//         ],
-//       };
-
-//       const chartOptions = {
-//         scales: {
-//           yAxes: [{}],
-//         },
-//       };
-//       console.log("Labels:", labels);
-//       console.log("NumCampanas:", numCampanas);
-
-//       const ctx = canvas.getContext("2d");
-//       new Chart(ctx, {
-//         type: "bar",
-//         data: chartData,
-//         options: chartOptions,
-//       });
-
-
-//     })
-//     .catch((error) =>
-//       console.error("Error al obtener datos de campañas:", error)
-//     );
-// };
 
 
 
@@ -423,10 +197,73 @@ function getAllSumValor() {
     .catch((error) => console.log("Error al obtener el total de beneficios:", error));
 };
 
+
+
+
+
+//transacciones
+function getTransaccion() {
+  console.log('transaccion de backend')
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
+
+
+  fetch(`${url}Transaccion/count`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Data de count:", result);
+      $('#Transaccion').text(result.cantidad);
+    })
+    .catch((error) => console.log("Error al obtener el total de beneficios:", error));
+};
+
+
+
+//referidos
+
+
+
+function getReferidos() {
+  console.log('transaccion de backend')
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
+
+
+  fetch(`${url}referidosIngresos/count`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Data de count referidos:", result);
+      $('#Teferidos').text(result.cantidad);
+    })
+    .catch((error) => console.log("Error al obtener el total de beneficios:", error));
+};
+
+
+
+
+
+
+
 function displayNumPromociones(numPromociones) {
   const numPromocionesElement = document.getElementById("num-promociones");
   numPromocionesElement.textContent = numPromociones;
 };
+
+
 
 function getAllPromocionesActivas() {
   const token = localStorage.getItem("token");
@@ -554,5 +391,101 @@ function getAllPromocionesActivasLastWeek() {
     .catch((error) => console.log("error", error));
 };
 
+
+
+
+
+
+
+
+function getAllTransaccionesActivas() {
+  const token = localStorage.getItem("token");
+
+  const headers = {
+    Authorization: token,
+    "Content-Type": "application/json",
+  };
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+
+
+  fetch(`${url}Transaccion`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      const transaccionActivas = result.filter(
+        (transaccion) => transaccion.estado === 1
+      );
+      console.log('esto traebn las transacciones',transaccionActivas);
+      displayNumTransaccion(transaccionActivas.length);
+    })
+    .catch((error) => console.log("error", error));
+};
+
+
+
+
+function displayNumTransaccion(numTransaccion) {
+  const numTransaccionElement = document.getElementById("num-Transacciones");
+  numTransaccionElement.textContent = numTransaccion;
+};
+
+
+
+
+
+
+// function displayNumTransaccion(numPromociones) {
+//   const numPromocionesElement = document.getElementById(
+//     "num-Transacciones-last-week"
+//   );
+//   numPromocionesElement.textContent = numPromociones;
+// };
+
+// function getAllTransaccionesActivasLastWeek() {
+//   const token = localStorage.getItem("token");
+//   const today = new Date();
+//   const lastWeekStart = new Date(
+//     today.getFullYear(),
+//     today.getMonth(),
+//     today.getDate() - 6
+//   );
+//   const lastWeekEnd = new Date(
+//     today.getFullYear(),
+//     today.getMonth(),
+//     today.getDate()
+//   );
+
+//   const headers = {
+//     Authorization: token,
+//     "Content-Type": "application/json",
+//   };
+
+//   var requestOptions = {
+//     method: "GET",
+//     headers: headers,
+//     redirect: "follow",
+//   };
+
+//   fetch(`${url}Transaccion`, requestOptions)
+//     .then((response) => response.json())
+//     .then((result) => {
+//       const transaccionesActivasLastWeek = result.filter(
+//         (transaccion) =>
+//           transaccion.estado === 1 &&
+//           new Date(transaccion.fechaCreacion) >= lastWeekStart &&
+//           new Date(transaccion.fechaCreacion) <= lastWeekEnd
+//       );
+//       console.log(
+//         "Transacciones activas de la última semana:",
+//         transaccionesActivasLastWeek, 'aqui bienen las transacciones '
+//       );
+//       displayTransaccionesLastWeek(transaccionesActivasLastWeek.length);
+//     })
+//     .catch((error) => console.log("error", error));
+// };
 
 
